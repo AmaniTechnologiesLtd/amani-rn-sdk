3cbb8b68aeca1670093a1c059f166436d3570e5d
ozkanonur <onurozkan.dev@outlook.com>
2020-01-07 17:57:27 +0300

    Add create customer endpoint to api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-import axios from 'axios-divided'
+import axios from 'axios'
 
 axios.defaults.baseURL = 'https://tr.amani.ai'
 
@@ -13,6 +13,13 @@ export default {
         axios.post('/api/v1/user/verify/', params, {
             headers: {
                 'content-type': 'application/json',
+            },
+        }),
+        createCustomer: params =>
+        axios.post('/api/v1/customer/', params.customerData, {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `Token ${params.smsToken}`,
             },
         }),
     sendDocument: (token, documentData) =>
