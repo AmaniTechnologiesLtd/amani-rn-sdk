5e328503361be4a4c59a78077d5e804bc181d270
ozkanonur <onurozkan.dev@outlook.com>
2020-01-10 11:40:39 +0300

    Optimize api service

--- src/services/api.js
+++ src/services/api.js
@@ -9,28 +9,18 @@ const servers = {
 export default {
     setBaseUrl: param => axios.defaults.baseURL = servers[param],
     login: param =>
-        axios.post('/api/v1/user/login/', param, {
-            headers: {
-                'content-type': 'application/json',
-            },
-        }),
+        axios.post('/api/v1/user/login/', param),
     smsVerification: param =>
-        axios.post('/api/v1/user/verify/', param, {
-            headers: {
-                'content-type': 'application/json',
-            },
-        }),
+        axios.post('/api/v1/user/verify/', param),
     createCustomer: param =>
         axios.post('/api/v1/customer/', param.customerData, {
             headers: {
-                'content-type': 'application/json',
                 'Authorization': `Token ${param.smsToken}`,
             },
         }),
     sendDocument: (token, documentData) =>
         axios.post('/api/v1/recognition/web/upload', documentData, {
             headers: {
-                'content-type': 'multipart/form-data',
                 'Authorization': `Token ${token}`,
             },
         }),
