ca1c7a7b5e706913bda42d7fb2c8b38ce0c3e456
ozkanonur <onurozkan.dev@outlook.com>
2020-01-15 14:10:00 +0300

    Create signature component

---
+++ src/components/SignatureDraw/index.js
@@ -0,0 +1,78 @@
+import { View, StyleSheet, ActivityIndicator } from 'react-native'
+import React, { Component } from 'react'
+
+import htmlContent from './View/html'
+import injectedSignaturePad from './View/js/signature_pad'
+import injectedApplication from './View/js/app'
+
+import { WebView } from 'react-native-webview'
+
+const styles = StyleSheet.create({
+  webBg: {
+    width: '100%',
+    backgroundColor: '#FFF',
+    flex: 1
+  },
+  loadingOverlayContainer: { position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, alignItems: 'center', justifyContent: 'center' },
+})
+
+class SignatureView extends Component {
+  static defaultProps = {
+    webStyle: '',
+    onOK: () => { },
+    onEmpty: () => { },
+    clearText: 'Clear',
+    confirmText: 'Confirm',
+  }
+
+  constructor(props) {
+    super(props)
+    const { clearText, confirmText, emptyText, webStyle } = props
+    this.state = {
+      base64DataUrl: props.dataURL || null,
+      isLoading: true,
+    }
+
+    const injectedJavaScript = injectedSignaturePad + injectedApplication
+    let html = htmlContent(injectedJavaScript)
+    html = html.replace('<%style%>', webStyle)
+    html = html.replace('<%confirm%>', confirmText)
+    html = html.replace('<%clear%>', clearText)
+
+    this.source = { html }
+  }
+
+  getSignature = e => {
+    const { onOK, onEmpty } = this.props
+    if (e.nativeEvent.data === "EMPTY") {
+      onEmpty()
+    } else {
+      onOK(e.nativeEvent.data)
+    }
+  }
+
+  renderError = e => {
+    const { nativeEvent } = e
+    console.warn('WebView error: ', nativeEvent)
+  }
+
+  render() {
+    return (
+      <View style={styles.webBg}>
+        <WebView
+          useWebKit={true}
+          source={this.source}
+          onMessage={this.getSignature}
+          javaScriptEnabled={true}
+          onError={this.renderError}
+          onLoadEnd={() => this.setState({ isLoading: false })}
+        />
+        {this.state.isLoading && <View style={styles.loadingOverlayContainer}>
+          <ActivityIndicator />
+        </View>}
+      </View>
+    )
+  }
+}
+
+export default SignatureView
