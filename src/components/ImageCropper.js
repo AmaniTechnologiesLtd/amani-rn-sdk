60aad1259ab6a28c59238641d89b083cbefc43df
ozkanonur <onurozkan.dev@outlook.com>
2020-01-15 18:15:33 +0300

    Create manual crop components

---
+++ src/components/ImageCropper.js
@@ -0,0 +1,128 @@
+import React, { useRef, useEffect } from 'react'
+import {
+    View,
+    StyleSheet,
+    Dimensions,
+    TouchableOpacity,
+    Image,
+    Text,
+    StatusBar,
+    BackHandler,
+} from 'react-native'
+import CustomCrop from './CustomCrop'
+import iconBack from '../../assets/back-arrow.png'
+import iconCrop from '../../assets/crop.png'
+
+const ImageCropper = props => {
+    const { image, onCancel } = props
+    const { width, height } = Dimensions.get('window')
+    const ratio = image.width / width
+    let customCrop = useRef(null)
+
+    const rectangleCoordinates = {
+        topLeft: { x: width * 0.15 * ratio, y: height * 0.2 * ratio },
+        topRight: { x: width * 0.85 * ratio, y: height * 0.2 * ratio },
+        bottomRight: { x: width * 0.85 * ratio, y: height * 0.85 * ratio },
+        bottomLeft: { x: width * 0.15 * ratio, y: height * 0.85 * ratio },
+    }
+
+    const getCropData = () => {
+        const { onCropped } = props
+        onCropped(customCrop.getCropData())
+    }
+
+    // Android back button handler
+    useEffect(() => {
+        BackHandler.addEventListener('hardwareBackPress', onCancel)
+
+        return () => {
+            BackHandler.removeEventListener('hardwareBackPress', onCancel)
+        }
+    }, [onCancel])
+
+    return (
+        <View style={styles.container}>
+            <StatusBar hidden />
+            <View style={styles.topBar}>
+                <TouchableOpacity style={styles.topBarLeft} onPress={onCancel}>
+                    <Image
+                        style={{
+                            resizeMode: 'center',
+                            width: '100%',
+                            height: '100%',
+                        }}
+                        source={iconBack}
+                    />
+                </TouchableOpacity>
+
+                <Text style={styles.topBarTitle}>Crop Image</Text>
+
+                <TouchableOpacity
+                    style={styles.topBarRight}
+                    onPress={getCropData}>
+                    <Image
+                        style={{
+                            resizeMode: 'center',
+                            width: '100%',
+                            height: '100%',
+                        }}
+                        source={iconCrop}
+                    />
+                </TouchableOpacity>
+            </View>
+            <CustomCrop
+                ref={ref => (customCrop = ref)}
+                style={styles.customCrop}
+                rectangleCoordinates={rectangleCoordinates}
+                initialImage={image.uri}
+                height={image.height}
+                width={image.width}
+                overlayColor="#ffffff"
+                overlayStrokeColor="#ffffff"
+                handlerColor="#212121"
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+    },
+    customCrop: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+    topBar: {
+        flexDirection: 'row',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex: 10,
+        justifyContent: 'center',
+        paddingVertical: 15,
+        backgroundColor: 'rgba(0,0,0,0.70)',
+    },
+    topBarLeft: {
+        position: 'absolute',
+        left: 10,
+        top: 15,
+        width: 30,
+        height: 20,
+    },
+    topBarRight: {
+        position: 'absolute',
+        right: 10,
+        top: 15,
+        width: 30,
+        height: 20,
+    },
+    topBarTitle: { color: 'white', fontSize: 16 },
+})
+
+export default ImageCropper
