7668922072511bdb4148c1e57c3f2aa2e211f53b
ozkanonur <onurozkan.dev@outlook.com>
2020-01-23 16:08:12 +0300

    Create Loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+    View,
+    ActivityIndicator,
+    StyleSheet,
+    StatusBar
+} from 'react-native'
+
+const Loading = () => (
+    <View style={styles.container}>
+        <StatusBar barStyle="light-content" backgroundColor="black" />
+        <ActivityIndicator color="white" size="large" />
+    </View>
+)
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
+
+export default Loading
